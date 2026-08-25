268d91ce6001096ec52da4286f4539d6364556ce
Parsa Tak <Parsaetak@gmail.com>
2026-08-26 02:51:42 +0330

    Simplify Home component by removing SceneRegistry
    Replaced SceneRegistry with LivingShell in Home component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
 import LivingShell from "@/components/LivingShell";
 import RedCursor from "@/components/RedCursor";
-import SceneRegistry from "@/components/SceneRegistry";
 
 export default function Home() {
   return (
     <>
       <RedCursor />
-
-      <LivingShell>
-        {(scene) => (
-          <SceneRegistry
-            scene={scene}
-          />
-        )}
-      </LivingShell>
+      <LivingShell />
     </>
   );
 }
