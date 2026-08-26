63fca23e220d5672f51dc82627e7f5eb371410be
Parsa Tak <Parsaetak@gmail.com>
2026-08-26 16:04:09 +0330

    Refactor NotFound component to use usePathname

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,32 @@
 "use client";
 
-import { useEffect } from "react";
+import {
+  useEffect
+} from "react";
+
+import {
+  usePathname
+} from "next/navigation";
 
 export default function NotFound() {
+  const pathname =
+    usePathname();
+
   useEffect(() => {
+    const segments =
+      pathname
+        .split("/")
+        .filter(Boolean);
+
+    const basePath =
+      segments.length > 0
+        ? `/${segments[0]}`
+        : "";
+
     window.location.replace(
-      `${window.location.origin}/WEB/`
+      `${window.location.origin}${basePath}/`
     );
-  }, []);
+  }, [pathname]);
 
   return null;
 }
