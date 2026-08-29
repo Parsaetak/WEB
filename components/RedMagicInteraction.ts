20dfe71c5eeb35fbf5fc26111fc91f64e3eed884
Parsa Tak <Parsaetak@gmail.com>
2026-08-29 13:03:23 +0330

    Add RedMagicInteraction types and emit function

---
+++ components/RedMagicInteraction.ts
@@ -0,0 +1,45 @@
+export type RedMagicInteractionType =
+  | "enter"
+  | "move"
+  | "impact"
+  | "flick"
+  | "charge"
+  | "release"
+  | "orbit"
+  | "leave";
+
+export type RedMagicInteractionDetail = {
+  type: RedMagicInteractionType;
+
+  x: number;
+  y: number;
+
+  velocity: number;
+  proximity: number;
+  energy: number;
+  angle: number;
+
+  /**
+   * 0..1 while the pointer is held.
+   * 0 for all other events.
+   */
+  charge: number;
+};
+
+export const RED_MAGIC_INTERACTION_EVENT =
+  "red-magic-interaction";
+
+export function emitRedMagicInteraction(
+  target: EventTarget,
+  detail: RedMagicInteractionDetail
+) {
+  target.dispatchEvent(
+    new CustomEvent<RedMagicInteractionDetail>(
+      RED_MAGIC_INTERACTION_EVENT,
+      {
+        detail,
+        bubbles: true
+      }
+    )
+  );
+}
