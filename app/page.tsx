1647a85727aea6b44125a35dde591afea3f4ae9b
Parsa Tak <Parsaetak@gmail.com>
2026-09-04 06:20:04 +0330

    Remove RedCursor component from Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 import LivingShell from "@/components/LivingShell";
-import RedCursor from "@/components/RedCursor";
 
 export default function Home() {
   return (
-    <>
-      <RedCursor />
-      <LivingShell />
-    </>
+    <LivingShell />
   );
 }
